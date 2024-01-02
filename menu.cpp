3b5d99b098cf625913473213f3950d997f969b95
Raúl Cano Esbrí <Ral Cano Esbr>
2024-01-02 15:49:11 +0100

    funcio llegir dades, funcio llegir usuaris, funcio usuaris nous -> fitxer usuaris.txt

--- menu.cpp
+++ menu.cpp
@@ -22,13 +22,15 @@ struct usuari{ //TUPLA ON ES GUARDEN LES DADES DE LA POBLACI�
 
 void llegirusuaris (int& nusuaris, vector<usuari>& usuaris);
 void llegirdades (vector<ciutat>& ciutats);
-void nouusuari ();
+void nouusuari (int& nusuaris, vector<usuari>& usuaris);
 void esborrarusuari ();
 void inicisesio(string& nomusuari, usuari& usuariactual);
+void finalitzar(int& nusuaris, vector<usuari>& usuaris);
+void menu1(int& seleccio, string& name, string& nomusuari, int& nusuaris, vector<usuari>& usuaris, vector<ciutat>& ciutats, usuari& usuariactual);
 
 int main (){
    //Declarar Variables
-   char seleccio;
+   int seleccio;
    string name, nomusuari;
    int nusuaris;
    vector<usuari> usuaris;
@@ -39,47 +41,19 @@ int main (){
    //Cridem accions inicialitzacio
    llegirdades(ciutats); //generara el vector amb totes les dades de les ciutats
    int tamany = ciutats.size();
-   cout<<"numero ciutats: "<<tamany;
-   //llegirusuaris(nusuaris, usuaris); //generara el vector amb tots els usuaris
-
-   //RECONEIXEMENT DE L'USUARI
-   cout<<"Et donem la benvinguda a la base de dades ___, on podras trobar informacio sobre la poblacio activa de diverses ciutats europees."<<endl;
-   cout<<"Si us plau, ingresa el teu nom: ";
-   cin>>name;
-   cout<<""<<endl;
-   cout<<"Hola "<<name<<"."<<endl;
-
-   //Menu Inicial
-   cout<<"Indica que vols fer escrivint el numero corresponent"<<endl;
-   cout<<"1. Iniciar Sesio"<<endl;
-   cout<<"2. Registrar Nou Usuari"<<endl;
-   cout<<"3. Esborrar Usuari"<<endl;
-   cout<<"4. Sortir"<<endl;
-
-   cout<<"Que vols fer?: ";
-   cin>>seleccio;
-   cout<<endl;
-
-   switch (seleccio){
-      case 1: inicisesio(string& nomusuari, usuari& usuariactual);
-         break;
-      case 2: nouusuari();
-         break;
-      case 3: esborrarusuari();
-         break;
-      case 4: cout<<"Gracies per utilitzar ___.";
-         break;
-      default: cout<<"Si us  plau, introdueix un numero valid"<<endl;
-   }//End switch
+   cout<<"numero ciutats: "<<tamany<<endl;
+   llegirusuaris(nusuaris, usuaris); //generara el vector amb tots els usuaris
+   menu1(seleccio, name, nomusuari, nusuaris, usuaris, ciutats, usuariactual);
+   finalitzar(nusuaris, usuaris);
 }//End main
 
-/*void llegirusuaris (int& nusuaris, vector<usuari>& usuaris){
-   wifstream UsuarisIN("usuaris.txt");
+void llegirusuaris (int& nusuaris, vector<usuari>& usuaris){
+   ifstream UsuarisIN("usuaris.txt");
    UsuarisIN>>nusuaris;
    for(int i=0; i<nusuaris; i++){
       usuari x;
       string ciutat;
-      UsuarisIN>>x.dni>>x.nom>>ciutat;
+      UsuarisIN>>x.nom>>x.contrasenya>>ciutat;
       while (ciutat != "FIN")
       {
          x.ciutatspreferides.push_back(ciutat);
@@ -88,7 +62,7 @@ int main (){
       usuaris.push_back(x);
    }
 }
-*/
+
 void llegirdades (vector<ciutat>& ciutats){
    ifstream Ptotal("poblacioactiva_total.csv");
    ifstream Phomes("poblacioactiva_homes.csv");
@@ -96,7 +70,7 @@ void llegirdades (vector<ciutat>& ciutats){
    string g, nomciutat;
    int any1, any2, any3, any4, any5, any6, any7, any8, any9, any10, pactivatotal, pactivahomes, pactivadones;
    Ptotal>>g>>any1>>any2>>any3>>any4>>any5>>any6>>any7>>any8>>any9>>any10;
-   cout<<g<<any1<<any2<<any3<<any4<<any5<<any6<<any7<<any8<<any9<<any10;
+   cout<<g<<" "<<any1<<" "<<any2<<" "<<any3<<" "<<any4<<" "<<any5<<" "<<any6<<" "<<any7<<" "<<any8<<" "<<any9<<" "<<any10<<endl;
    while(Ptotal>>nomciutat){
       ciutat x;
       x.nom = nomciutat;
@@ -114,7 +88,8 @@ void llegirdades (vector<ciutat>& ciutats){
    }
 }
 
-void nouusuari (){
+void nouusuari (int& nusuaris, vector<usuari>& usuaris){
+   cout<<endl<<"Numero d'usuaris: "<<usuaris.size()<<endl;
    string username, password;
    cout<<"Introdueix el teu nom d'usuari, sense fer servir espais."<<endl<<"Nom d'usuari: ";
    cin>>username;
@@ -124,6 +99,8 @@ void nouusuari (){
    x.nom = username;
    x.contrasenya = password;
    usuaris.push_back(x);
+   cout<<endl<<"Numero d'usuaris: "<<usuaris.size()<<endl;
+   nusuaris = usuaris.size();
 }
 
 void esborrarusuari (){
@@ -142,4 +119,47 @@ void inicisesio(string& nomusuari, usuari& usuariactual){
    cout<<endl<<"Contrasenya: ";
    cin>>password;
    nomusuari = username;
+}
+
+void menu1(int& seleccio, string& name, string& nomusuari, int& nusuaris, vector<usuari>& usuaris, vector<ciutat>& ciutats, usuari& usuariactual){
+   cout<<"Et donem la benvinguda a la base de dades ___, on podras trobar informacio sobre la poblacio activa de diverses ciutats europees."<<endl;
+   cout<<"Si us plau, ingresa el teu nom: ";
+   cin>>name;
+   cout<<""<<endl;
+   cout<<"Hola "<<name<<"."<<endl;
+
+   //Menu Inicial
+   cout<<"Indica que vols fer escrivint el numero corresponent"<<endl;
+   cout<<"1. Iniciar Sesio"<<endl;
+   cout<<"2. Registrar Nou Usuari"<<endl;
+   cout<<"3. Esborrar Usuari"<<endl;
+   cout<<"4. Sortir"<<endl;
+
+   cout<<"Que vols fer?: ";
+   cin>>seleccio;
+   cout<<endl;
+
+   switch (seleccio){
+      case 1: inicisesio(nomusuari, usuariactual);
+         break;
+      case 2: nouusuari(nusuaris, usuaris);
+         break;
+      case 3: esborrarusuari();
+         break;
+      case 4: finalitzar(nusuaris, usuaris); cout<<"Gracies per utilitzar ___.";
+         break;
+      default: cout<<"Si us  plau, introdueix un numero valid"<<endl;
+   }//End switch
+}
+
+void finalitzar(int& nusuaris, vector<usuari>& usuaris){
+   ofstream UsuarisOUT("usuaris.txt");
+   UsuarisOUT<<nusuaris<<endl;
+   for(int i = 0; i<nusuaris; i++){
+      UsuarisOUT<<usuaris[i].nom<<" "<<usuaris[i].contrasenya<<" ";
+      for(int nciutats = 0; nciutats < usuaris[i].ciutatspreferides.size(); nciutats++){
+         UsuarisOUT<<usuaris[i].ciutatspreferides[nciutats]<<" ";
+      }
+      UsuarisOUT<<"FIN"<<endl;
+   }
 }
